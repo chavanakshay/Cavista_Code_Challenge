9f484328938628a065d34fd23cdf57dee886216b
Akshay  Chavan <akshay.chavan@fujitsu.com>
2020-10-08 13:59:14 -0700

    Initial Commit

---
+++ Cavista Code ChallengeTests/Cavista_Code_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cavista_Code_ChallengeTests.swift
+//  Cavista Code ChallengeTests
+//
+//  Created by Akshay  Chavan on 08/10/20.
+//  Copyright © 2020 Akshay  Chavan. All rights reserved.
+//
+
+import XCTest
+@testable import Cavista_Code_Challenge
+
+class Cavista_Code_ChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
